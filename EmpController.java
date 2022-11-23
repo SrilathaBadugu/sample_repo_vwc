3445e4e0fbe8c192fe7c58e87b77765ecc7d05c6
SrilathaBadugu <118732256+SrilathaBadugu@users.noreply.github.com>
2022-11-23 17:32:02 +0530

    file uploaded
    file uploaded and commit changes

---
+++ EmpController.java
@@ -0,0 +1,45 @@
+package com.sb.EmpController;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sb.EmpService.EmpService;
+@RestController
+@Component
+public class EmpController {
+	
+		private static final Logger logger = LoggerFactory.getLogger(EmpService.class);
+	    @Autowired
+	    EmpService s;
+	    
+		@RequestMapping("/v1")
+		public String Show() {
+		logger.info("This is First Method");
+			return "Hello";
+		}
+
+		@RequestMapping("/v2")
+		public String print() {
+		logger.info("This is Second Method");
+			return "Hello Vilage World Class";
+		}
+		
+		@RequestMapping("/v3")
+		public ArrayList load() {
+        logger.info("This is Third Method");
+			return s.m1();
+		}
+		
+		}
+
+
+		
+	
+
+
